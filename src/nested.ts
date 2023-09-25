eed1640aa1a072a20b2f2f2617edf7af506ec249
Leon Giang <philong@udel.edu>
2023-09-25 13:23:32 -0400

    Finished sumPublishedPoints()

--- src/nested.ts
+++ src/nested.ts
@@ -88,7 +88,17 @@ export function sumPoints(questions: Question[]): number {
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    const publishedQuestions = questions.filter(
+        (question: Question): boolean => question.published
+    );
+
+    const sumPublishedPoints = publishedQuestions.reduce(
+        (currentSum: number, question: Question) =>
+            currentSum + question.points,
+        0
+    );
+
+    return sumPublishedPoints;
 }
 
 /***
