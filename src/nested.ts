3d50a1a06f4b0c1c38441da9fcf4cc56f8db6f95
Leon Giang <philong@udel.edu>
2023-09-25 13:32:55 -0400

    finished toCSV()

--- src/nested.ts
+++ src/nested.ts
@@ -119,7 +119,18 @@ id,name,options,points,published
  * Check the unit tests for more examples!
  */
 export function toCSV(questions: Question[]): string {
-    return "";
+    const headers = "id,name,options,points,published\n";
+
+    const csvBody = questions
+        .map(
+            (question: Question): string =>
+                `${question.id},${question.name},${question.options.length},${question.points},${question.published}`
+        )
+        .join("\n");
+
+    const csvTotal = headers + csvBody;
+
+    return csvTotal;
 }
 
 /**
