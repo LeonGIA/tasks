aa1ba6f075efc954fb06290607dc25026a8c757f
Leon Giang <philong@udel.edu>
2023-09-27 20:59:12 -0400

    Complete CycleHolidy Component

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,55 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+type Holiday =
+    | "Christmas"
+    | "Independence Day"
+    | "New Year"
+    | "Thanksgiving"
+    | "Memorial Day";
+
+const holidayEmojis: Record<Holiday, string> = {
+    Christmas: "🎄",
+    "Independence Day": "🗽",
+    "New Year": "🎇",
+    Thanksgiving: "🦃",
+    "Memorial Day": "🦅"
+};
+
+const holidayAlphabet: Record<Holiday, Holiday> = {
+    Christmas: "Independence Day",
+    "Independence Day": "Memorial Day",
+    "Memorial Day": "New Year",
+    "New Year": "Thanksgiving",
+    Thanksgiving: "Christmas"
+};
+
+const holidayYear: Record<Holiday, Holiday> = {
+    "New Year": "Memorial Day",
+    "Memorial Day": "Independence Day",
+    "Independence Day": "Thanksgiving",
+    Thanksgiving: "Christmas",
+    Christmas: "New Year"
+};
+
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<Holiday>("New Year");
+
+    function changeHolidayAlphabet(): void {
+        const nextHoliday = holidayAlphabet[holiday];
+        setHoliday(nextHoliday);
+    }
+
+    function changeHolidayYear(): void {
+        const nextHoliday = holidayYear[holiday];
+        setHoliday(nextHoliday);
+    }
+
+    return (
+        <div>
+            <span>Holiday: {holidayEmojis[holiday]}</span>
+            <button onClick={changeHolidayAlphabet}>Advance by Alphabet</button>
+            <button onClick={changeHolidayYear}>Advance by Year</button>
+        </div>
+    );
 }
