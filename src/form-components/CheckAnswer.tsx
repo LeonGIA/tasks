ab5f7df015d0140d1337ef240a8d6056454c67ab
Leon Giang <philong@udel.edu>
2023-10-04 13:19:38 -0400

    Finished CheckAnswer Component

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,27 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [userAnswer, setUserAnswer] = useState<string>("");
+
     return (
         <div>
             <h3>Check Answer</h3>
+            <Form.Group controlId="formCheckAnswer">
+                <Form.Label>Enter an Answer:</Form.Label>
+                <Form.Control
+                    type="string"
+                    value={userAnswer}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setUserAnswer(event.target.value)
+                    }
+                ></Form.Control>
+            </Form.Group>
+            <span>{userAnswer === expectedAnswer ? "✔️" : "❌"}</span>
         </div>
     );
 }
