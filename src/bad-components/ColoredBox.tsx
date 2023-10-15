975c99e762f707c441b2b83c53383131a6928969
Leon Giang <philong@udel.edu>
2023-10-14 23:09:11 -0400

    Fixed ColoredBox and ShoveBox components to use props.

--- src/bad-components/ColoredBox.tsx
+++ src/bad-components/ColoredBox.tsx
@@ -4,6 +4,39 @@ import { Button } from "react-bootstrap";
 export const COLORS = ["red", "blue", "green"];
 const DEFAULT_COLOR_INDEX = 0;
 
+interface BoxColor {
+    setColorIndex: (index: number) => void;
+    colorIndex: number;
+}
+
+function ChangeColor(props: BoxColor): JSX.Element {
+    return (
+        <Button
+            onClick={() =>
+                props.setColorIndex((1 + props.colorIndex) % COLORS.length)
+            }
+        >
+            Next Color
+        </Button>
+    );
+}
+
+function ColorPreview(props: BoxColor): JSX.Element {
+    return (
+        <div
+            data-testid="colored-box"
+            style={{
+                width: "50px",
+                height: "50px",
+                backgroundColor: COLORS[props.colorIndex],
+                display: "inline-block",
+                verticalAlign: "bottom",
+                marginLeft: "5px"
+            }}
+        ></div>
+    );
+}
+
 export function ColoredBox(): JSX.Element {
     const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
     return (
@@ -11,24 +44,14 @@ export function ColoredBox(): JSX.Element {
             <h3>Colored Box</h3>
             <span>The current color is: {COLORS[colorIndex]}</span>
             <div>
-                <Button
-                    onClick={() =>
-                        setColorIndex((1 + colorIndex) % COLORS.length)
-                    }
-                >
-                    Next Color
-                </Button>
-                <div
-                    data-testid="colored-box"
-                    style={{
-                        width: "50px",
-                        height: "50px",
-                        backgroundColor: COLORS[colorIndex],
-                        display: "inline-block",
-                        verticalAlign: "bottom",
-                        marginLeft: "5px"
-                    }}
-                ></div>
+                <ChangeColor
+                    setColorIndex={setColorIndex}
+                    colorIndex={colorIndex}
+                ></ChangeColor>
+                <ColorPreview
+                    setColorIndex={setColorIndex}
+                    colorIndex={colorIndex}
+                ></ColorPreview>
             </div>
         </div>
     );
--- src/bad-components/ShoveBox.tsx
+++ src/bad-components/ShoveBox.tsx
@@ -1,31 +1,53 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+interface BoxPosition {
+    setBoxPosition: (position: number) => void;
+    boxPosition: number;
+}
+
+function ShoveBoxButton(props: BoxPosition) {
+    return (
+        <Button onClick={() => props.setBoxPosition(4 + props.boxPosition)}>
+            Shove the Box
+        </Button>
+    );
+}
+
+function MoveableBox(props: BoxPosition): JSX.Element {
+    return (
+        <div
+            data-testid="moveable-box"
+            style={{
+                width: "50px",
+                height: "50px",
+                backgroundColor: "lightblue",
+                border: "1px solid blue",
+                display: "inline-block",
+                verticalAlign: "bottom",
+                marginLeft: props.boxPosition + "px"
+            }}
+        ></div>
+    );
+}
+
 export function ShoveBox(): JSX.Element {
     const [position, setPosition] = useState<number>(10);
-
     return (
         <div>
             <h3>Shove Box</h3>
             <span>The box is at: {position}</span>
             <div>
-                <Button onClick={() => setPosition(4 + position)}>
-                    Shove the Box
-                </Button>
+                <ShoveBoxButton
+                    boxPosition={position}
+                    setBoxPosition={setPosition}
+                ></ShoveBoxButton>
+                <br />
+                <MoveableBox
+                    boxPosition={position}
+                    setBoxPosition={setPosition}
+                ></MoveableBox>
             </div>
-
-            <div
-                data-testid="moveable-box"
-                style={{
-                    width: "50px",
-                    height: "50px",
-                    backgroundColor: "lightblue",
-                    border: "1px solid blue",
-                    display: "inline-block",
-                    verticalAlign: "bottom",
-                    marginLeft: position + "px"
-                }}
-            ></div>
         </div>
     );
 }
