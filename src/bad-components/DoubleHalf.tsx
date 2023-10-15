b08b08658ea6e344cefecaff09c30d2f4a292efa
Leon Giang <philong@udel.edu>
2023-10-14 23:20:52 -0400

    Fixed DoubleHalf so that it uses props too.

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,16 +1,33 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+interface Value {
+    setValue: (value: number) => void;
+    value: number;
+}
+
+function Doubler(props: Value): JSX.Element {
+    return (
+        <Button onClick={() => props.setValue(2 * props.value)}>Double</Button>
+    );
+}
+
+function Halver(props: Value): JSX.Element {
+    return (
+        <Button onClick={() => props.setValue(0.5 * props.value)}>Halve</Button>
+    );
+}
+
 export function DoubleHalf(): JSX.Element {
-    const [dhValue, setDhValue] = useState<number>(10);
+    const [value, setValue] = useState<number>(10);
     return (
         <div>
             <h3>Double Half</h3>
             <div>
-                The current value is: <span>{dhValue}</span>
+                The current value is: <span>{value}</span>
             </div>
-            <Button onClick={() => setDhValue(2 * dhValue)}>Double</Button>
-            <Button onClick={() => setDhValue(0.5 * dhValue)}>Halve</Button>
+            <Doubler setValue={setValue} value={value}></Doubler>
+            <Halver setValue={setValue} value={value}></Halver>
         </div>
     );
 }
